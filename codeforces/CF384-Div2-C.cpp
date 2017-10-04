63c7e0f286c4f891badedea0d70e036a0143ff08
mostafa abobakr <mostafa.abobakr62@yahoo.com>
2017-10-04 17:27:27 +0200

    Create CF384-Div2-C.cpp

---
+++ codeforces/CF384-Div2-C.cpp
@@ -0,0 +1,81 @@
+#include<set>
+#include<map>
+#include<list>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<complex>
+#include<sstream>
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<numeric>
+#include<utility>
+#include<functional>
+#include<stdio.h>
+#include<assert.h>
+#include<memory.h>
+#include<bitset>
+using namespace std;
+
+#define all(v)				((v).begin()), ((v).end())
+#define sz(v)				((int)((v).size()))
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define rep(i, v)		for(int i=0;i<sz(v);++i)
+#define lp(i, n)		for(int i=0;i<(int)(n);++i)
+#define lpi(i, j, n)	for(int i=(j);i<(int)(n);++i)
+#define lpd(i, j, n)	for(int i=(j);i>=(int)(n);--i)
+#define pb					push_back
+#define MP					make_pair
+#define P(x)				cout<<#x<<" = { "<<x<<" }\n"
+typedef long long         ll;
+typedef long double   	  ld;
+typedef vector<int>       vi;
+typedef vector<double>    vd;
+typedef vector< vi >      vvi;
+typedef vector< vd >      vvd;
+typedef vector<string>    vs;
+typedef vector<ll>		  vll;
+
+
+const ll OO = 1e17;
+
+const double EPS = (1e-7);
+int dcmp(double x, double y) { return fabs(x - y) <= EPS ? 0 : x < y ? -1 : 1; }
+
+
+void fast(){
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+}
+int n;
+int a[(int)2e5 + 2];
+int o, z;
+int main(){
+	fast();
+	cin >> n;
+	for (int i = 0; i < n; i++){
+		cin >> a[i];
+		o += (a[i]);
+		z += (!a[i]);
+	}
+	ll cnt = 0;
+	if (o >= z){
+		for (int i = n - 1; i >= 0; i--){
+			if (a[i])o--;
+			if (!a[i])cnt += o;
+		}
+	}
+	else {
+		for (int i = 0; i < n;i++){
+			if (!a[i])z--;
+			if (a[i])cnt += z;
+		}
+	}
+	cout << cnt << endl;
+	return 0;
+}
+//mosta7il_
