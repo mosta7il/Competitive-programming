7ed95b9d5050dfcab415fe7ad0d586654e9a8ac4
mostafa abobakr <mostafa.abobakr62@yahoo.com>
2017-10-18 02:46:48 +0200

    Create 12751 - An Interesting Game.cpp

---
+++ uva/12751 - An Interesting Game.cpp
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<set>
+#include<map>
+#include<list>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<complex>
+#include<sstream>
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<numeric>
+#include<utility>
+#include<functional>
+#include<stdio.h>
+#include<assert.h>
+#include<memory.h>
+#include<bitset>
+using namespace std;
+
+#define all(v)				((v).begin()), ((v).end())
+#define sz(v)				((int)((v).size()))
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define rep(i, v)		for(int i=0;i<sz(v);++i)
+#define lp(i, n)		for(int i=0;i<(int)(n);++i)
+#define lpi(i, j, n)	for(int i=(j);i<(int)(n);++i)
+#define lpd(i, j, n)	for(int i=(j);i>=(int)(n);--i)
+#define pb					push_back
+#define MP					make_pair
+#define P(x)				cout<<#x<<" = { "<<x<<" }\n"
+typedef long long         ll;
+typedef long double   	  ld;
+typedef vector<int>       vi;
+typedef vector<double>    vd;
+typedef vector< vi >      vvi;
+typedef vector< vd >      vvd;
+typedef vector<string>    vs;
+typedef vector<ll>		  vll;
+
+
+const ll OO = 1e16;
+
+const double EPS = (1e-7);
+int dcmp(double x, double y) { return fabs(x - y) <= EPS ? 0 : x < y ? -1 : 1; }
+
+
+void fast(){
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+}
+
+int t;
+int main(){
+	fast();
+	cin >> t;
+	int c = 0;
+	while (t--){
+		ll n, k, x;
+		cin >> n >> k >> x;
+		ll res = (n*(n + 1) / 2)+(x*(x-1)/2)-((x+k-1)*(x+k)/2);
+		cout <<"Case "<<++c<<": "<< res << endl;
+	}
+	return 0;
+}
+//mosta7il_
