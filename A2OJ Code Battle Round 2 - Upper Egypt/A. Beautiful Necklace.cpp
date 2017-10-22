8d56ad0adafaa892ac3db55fbd45e0eb376c57ec
mostafa abobakr <mostafa.abobakr62@yahoo.com>
2017-10-22 15:10:35 +0200

    Create A. Beautiful Necklace.cpp

---
+++ A2OJ Code Battle Round 2 - Upper Egypt/A. Beautiful Necklace.cpp
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<set>
+#include<map>
+#include<list>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<complex>
+#include<sstream>
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<numeric>
+#include<utility>
+#include<functional>
+#include<stdio.h>
+#include<assert.h>
+#include<memory.h>
+#include<bitset>
+using namespace std;
+
+#define all(v)				((v).begin()), ((v).end())
+#define sz(v)				((int)((v).size()))
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define rep(i, v)		for(int i=0;i<sz(v);++i)
+#define lp(i, n)		for(int i=0;i<(int)(n);++i)
+#define lpi(i, j, n)	for(int i=(j);i<(int)(n);++i)
+#define lpd(i, j, n)	for(int i=(j);i>=(int)(n);--i)
+#define pb					push_back
+#define MP					make_pair
+#define P(x)				cout<<#x<<" = { "<<x<<" }\n"
+typedef long long         ll;
+typedef long double   	  ld;
+typedef vector<int>       vi;
+typedef vector<double>    vd;
+typedef vector< vi >      vvi;
+typedef vector< vd >      vvd;
+typedef vector<string>    vs;
+typedef vector<ll>		  vll;
+
+
+const ll OO = 1e16;
+
+const double EPS = (1e-7);
+int dcmp(double x, double y) { return fabs(x - y) <= EPS ? 0 : x < y ? -1 : 1; }
+
+
+void fast(){
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+}
+
+int n;
+string a[51];
+int main(){
+	fast();
+	cin >> n;
+	int mx = 0;
+	for (int i = 0; i < n; i++){
+		int cnt = 1;
+		bool f = 0;
+		cin >> a[i];
+		lp(j, a[i].size()-1){
+			if (a[i][j] == a[i][j + 1])cnt++;
+			else{
+				f = 1;
+				break;
+			}
+		}
+		if (!f){
+			mx = max(cnt, mx);
+		}
+	}
+
+	cout << mx << endl;
+	return 0;
+}
+//mosta7il_
