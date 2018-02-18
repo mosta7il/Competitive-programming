169230cb46657444ef2f210249098ce6ed55a863
mostafa abobakr <mostafa.abobakr62@yahoo.com>
2018-02-18 17:27:37 +0200

    Create CF199-D2-C.cpp

---
+++ codeforces/CF199-D2-C.cpp
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<set>
+#include<map>
+#include<list>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<complex>
+#include<sstream>
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<numeric>
+#include<utility>
+#include<functional>
+#include<stdio.h>
+#include<assert.h>
+#include<memory.h>
+#include<bitset>
+using namespace std;
+
+#define all(v)				((v).begin()), ((v).end())
+#define sz(v)				((int)((v).size()))
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define rep(i, v)		for(int i=0;i<sz(v);++i)
+#define lp(i, n)		for(int i=0;i<(int)(n);++i)
+#define lpi(i, j, n)	for(int i=(j);i<(int)(n);++i)
+#define lpd(i, j, n)	for(int i=(j);i>=(int)(n);--i)
+#define pb					push_back
+#define MP					make_pair
+#define P(x)				cout<<#x<<" = { "<<x<<" }\n"
+typedef long long         ll;
+typedef long double   	  ld;
+typedef vector<int>       vi;
+typedef vector<double>    vd;
+typedef vector< vi >      vvi;
+typedef vector< vd >      vvd;
+typedef vector<string>    vs;
+typedef vector<ll>		  vll;
+
+
+const ll OO = 1e16;
+
+const double EPS = (1e-7);
+int dcmp(double x, double y) { return fabs(x - y) <= EPS ? 0 : x < y ? -1 : 1; }
+
+
+
+void fast(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	
+	#endif
+
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+}
+ll k , b , n , t;
+
+int main(){
+	fast();
+	cin>>k>>b>>n>>t;
+	if (k == 1) return cout << max((b * n - t + b) / b, 0LL) << endl,0;
+	ll lhs = (t * (k-1) + b);
+	ll pwr =1;
+	for(int i = 0 ;;i++){
+		if(lhs < (pwr*(k-1+b)) ){
+			cout<<max(0ll,n-i+1)<<endl;
+			return 0;
+		}
+		pwr*=k;
+	}
+
+	return 0;
+}
+//mosta7il_;
