81a5ac1d633f7285e333ce4c479ade325fbfa911
mostafa abobakr <mostafa.abobakr62@yahoo.com>
2018-03-03 15:12:44 +0200

    Create CF474-D2-D.cpp

---
+++ codeforces/CF474-D2-D.cpp
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<set>
+#include<map>
+#include<list>
+#include<iomanip>
+#include<cmath>
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<complex>
+#include<sstream>
+#include<iostream>
+#include<fstream>
+#include<algorithm>
+#include<numeric>
+#include<utility>
+#include<functional>
+#include<stdio.h>
+#include<assert.h>
+#include<memory.h>
+#include<bitset>
+using namespace std;
+
+#define all(v)				((v).begin()), ((v).end())
+#define sz(v)				((int)((v).size()))
+#define clr(v, d)			memset(v, d, sizeof(v))
+#define rep(i, v)		for(int i=0;i<sz(v);++i)
+#define lp(i, n)		for(int i=0;i<(int)(n);++i)
+#define lpi(i, j, n)	for(int i=(j);i<(int)(n);++i)
+#define lpd(i, j, n)	for(int i=(j);i>=(int)(n);--i)
+#define pb					push_back
+#define MP					make_pair
+#define P(x)				cout<<#x<<" = { "<<x<<" }\n"
+typedef long long         ll;
+typedef long double   	  ld;
+typedef vector<int>       vi;
+typedef vector<double>    vd;
+typedef vector< vi >      vvi;
+typedef vector< vd >      vvd;
+typedef vector<string>    vs;
+typedef vector<ll>		  vll;
+
+
+const ll OO = 1e9;
+const ll mod = 1000000007;
+const double EPS = (1e-7);
+int dcmp(double x, double y) { return fabs(x - y) <= EPS ? 0 : x < y ? -1 : 1; }
+
+
+void fast(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	
+	#endif
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+}
+int t , k;
+int a , b;
+ll mem[100001];
+
+int main(){
+	fast();
+	
+	cin>>t>>k;
+	mem[0] =1;
+	for(int i = 1;i<=100000;i++){
+		if(i - k >= 0)
+			mem[i]+=(mem[i-k]%mod);;
+			mem[i]%=mod;
+		mem[i]+=(mem[i-1]%mod);
+		mem[i]%=mod;
+	}
+	mem[0] = 0;
+	for(int i = 1;i<=100000;i++){
+		mem[i]+=(mem[i-1]%mod);
+		mem[i]%=mod;
+	}
+
+	while(t--){
+		cin>>a>>b;
+		cout<<(mem[b] - mem[a-1] + mod)%mod<<endl;
+	}
+	return 0;
+}
