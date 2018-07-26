fe7f802526c7b821383303141c056269f3ac0f3f
mostafa abobakr <mostafa.abobakr62@gmail.com>
2018-07-26 10:32:47 +0200

    Create 10110 - Light, more light.cpp

---
+++ uva/10110 - Light, more light.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+
+const long long OO = 1e9;
+const long long mod = 1e9 + 7;
+const long double EPS = (1e-18);
+int dcmp(long double x, long double y) { return fabs(x - y) <= EPS ? 0 : x < y ? -1 : 1; }
+
+int dx[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+
+void fast(){
+#ifndef ONLINE_JUDGE
+	freopen("F:\\solving\\input.txt", "r", stdin);
+	freopen("F:\\solving\\output.txt", "w", stdout);
+
+#endif
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+}
+const long long MAX = 20 *1000 * 1000 + 1;
+long long n;
+
+int main(){
+	fast();
+	while (cin >> n&&n){
+		int cnt = 0;
+		long long sr = sqrt(n);
+		if (sr * sr != n){
+			cout << "no\n";
+		}
+		else 
+			cout << "yes\n";
+	}
+	
+	return 0;
+}
