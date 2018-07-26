a1f4ec71840fb73d544bfa752b3b998069f4189e
mostafa abobakr <mostafa.abobakr62@gmail.com>
2018-07-26 20:20:39 +0200

    Create 11847 - Cut the Silver Bar.cpp

---
+++ uva/11847 - Cut the Silver Bar.cpp
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+
+const long long OO = 1e9;
+const long long mod = 1e9 + 7;
+const long double EPS = (1e-18);
+int dcmp(long double x, long double y) { return fabs(x - y) <= EPS ? 0 : x < y ? -1 : 1; }
+
+int dx[] = { 0, 0, 1, -1, 1, 1, -1, -1 };
+int dy[] = { 1, -1, 0, 0, -1, 1, 1, -1 };
+
+
+void fast(){
+#ifndef ONLINE_JUDGE
+	freopen("F:\\solving\\input.txt", "r", stdin);
+	freopen("F:\\solving\\output.txt", "w", stdout);
+
+#endif
+	std::ios_base::sync_with_stdio(0);
+	cin.tie(NULL); cout.tie(NULL);
+}
+const long long MAX = 20 *1000 * 1000 + 1;
+
+int n;
+
+int main(){
+	fast();
+	while (cin >> n && n){
+
+		int idx = 0, mask = 0;
+		while ((mask | (1 << idx)) < n){
+			mask |= (1 << idx);
+			idx++;
+		}
+		cout << idx << endl;
+	}
+	return 0;
+}
